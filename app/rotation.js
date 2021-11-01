f18e8de1ffa01ef3f670b1e388740f6e5938a5a6
dahool <dahool@users.noreply.github.com>
2021-11-01 08:27:53 -0300

    update dailies rotation

--- app/rotation.js
+++ app/rotation.js
@@ -4,16 +4,18 @@ const fs = require('fs');
 
 const rotationFile = './commands/dailiesrotation.json'
 
-let start = DateTime.fromObject({ year: 2021, month: 8, day: 30})
+let start = DateTime.fromObject({ year: 2021, month: 11, day: 1})
+let firstRotation = 6;
 const stop = start.plus({days: 730});
 
 const days = [];
 
 while (start < stop) {
-	for (let i = 1; i <= 11; i++) {
+	for (let i = firstRotation; i <= 11; i++) {
 		days.push({rotation: i, rotationDay: start.toFormat('yyyy-MM-dd')})
 		start = start.plus({days: 1});
 	}
+	firstRotation = 1;
 }
 
 let data = JSON.stringify(days);
