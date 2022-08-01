b9aaf24d4ebbc09c21680ac428ad4096dad78f42
dahool <dahool@users.noreply.github.com>
2022-08-01 15:45:09 -0300

    minor update

--- app/functions/announcer.js
+++ app/functions/announcer.js
@@ -33,7 +33,7 @@ createMessage = (data) => {
 
     const message = new Discord.MessageEmbed()
         .setColor(randomColor())
-        .setTitle(data.summary + ' [' + data.location + ']')
+        .setTitle(data.summary)
         .setURL('https://www.timeanddate.com/countdown/generic?p0=1440&iso=' + startTime.setZone('UTC').toISO() + "&msg=" + encodeURIComponent(data.summary))
         .setDescription(data.description || ' ')
         .setThumbnail('https://www.dropbox.com/s/6jzlixqvk4nhpg9/redalert.gif?raw=1')
@@ -78,7 +78,7 @@ module.exports = {
                     if (cfg) {
                         console.log(cfg);
                         const channel = client.client.guilds.cache.get(cfg.guild).channels.cache.get(cfg.channel);
-                        if (channel) sendMessage(e.summary, (e.mentions && e.mentions.length > 0 ? e.mentions : cfg.mention) || [], channel, createMessage(e));
+                        if (channel) sendMessage(e.summary  + ' [' + e.location + ']', (e.mentions && e.mentions.length > 0 ? e.mentions : cfg.mention) || [], channel, createMessage(e));
                     }
                 })
             });
