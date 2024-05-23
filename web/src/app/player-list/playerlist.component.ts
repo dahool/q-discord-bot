703df926d48a9988850d6c54f12fea2f8d6f22af
dahool <dahool@users.noreply.github.com>
2024-05-23 17:30:13 -0300

    player list export

--- web/src/app/player-list/playerlist.component.ts
+++ web/src/app/player-list/playerlist.component.ts
@@ -1,6 +1,8 @@
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { saveAs } from 'file-saver';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { AppService } from '../service/app-services.service';
 
 interface Query {
@@ -24,7 +26,10 @@ export class PlayerListComponent implements OnInit {
 
   resultList: any[] = [];
 
-  constructor(private service: AppService) {}
+  constructor(
+    private service: AppService,
+    protected spinner: NgxSpinnerService
+  ) {}
 
   ngOnInit(): void {
     this.service.getPlayerInfoVersions().subscribe(v => this.versions = v.values.sort().reverse());
@@ -33,6 +38,16 @@ export class PlayerListComponent implements OnInit {
 
   search() {
     this.service.getPlayerList(this.query).subscribe(l => this.resultList = l);
+  }
+
+  exportList() {
+    this.spinner.show();
+    const header = ['name', 'level', 'tag', 'power', 'pd', 'rss', 'version'];
+    let csv = this.resultList.map(o => `${o.name},${o.level},${o.tag},${o.power},${o.pd},${o.rss},${o.version}`);
+    let csvArray = [header].concat(csv).join('\r\n');
+    var blob = new Blob([csvArray], {type: 'text/csv' })
+    saveAs(blob, "playerlist.csv");
+    this.spinner.hide();
   }
 
 }
